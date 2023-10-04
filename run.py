501bfab64abdb33bf4181f497347273f32b2d580
LanreAndero <>
2023-10-04 14:39:30 +0000

    add more questions and answers to list of dictionaries

--- run.py
+++ run.py
@@ -6,37 +6,49 @@ import random
 quiz_data = [
     {
         "question": "Which river is the longest in Africa?",
-        "options": ["Congo River", "Nile River", "Niger River", "Zambezi"],
+        "options": [
+            "Congo River", "Nile River", "Niger River", "Zambezi River"
+        ],
         "correct_answer": "Nile River",
     },
     {
         "question": "Which African country is known as the 'Rainbow Nation'?",
-        "options": ["Nigeria", "Kenya", "South Africa", "Egypt"],
+        "options": [
+            "Nigeria", "Kenya", "South Africa", "Egypt"
+        ],
         "correct_answer": "South Africa",
     },
     {
-        "question": "Which African nation was never colonized by European?",
+        "question": "Which African nation was never colonized by European",
+        "question_contd": "powers?",
         "options": ["Ghana", "Ethiopia", "Nigeria", "South Africa"],
         "correct_answer": "Ethiopia",
     },
     {
-        "question": "Which African country is famous for its safaris?",
+        "question": "Which African country is famous for its wildlife and",
+        "question_contd": "safaris?",
         "options": ["Ghana", "Tanzania", "Ethiopia", "Mali"],
         "correct_answer": "Tanzania",
     },
     {
         "question": "What is the largest lake in Africa?",
-        "options": ["Lake Chad", "Lake Malawi", "Tanganyika", "Lake Victoria"],
+        "options": [
+            "Lake Chad", "Lake Malawi", "Lake Tanganyika", "Lake Victoria"
+        ],
         "correct_answer": "Lake Victoria",
     },
     {
         "question": "Which African country is 'Land of a Thousand Hills'?",
-        "options": ["Rwanda", "Uganda", "Burundi", "Togo"],
+        "options": [
+            "Rwanda", "Uganda", "Burundi", "Togo"
+        ],
         "correct_answer": "Rwanda",
     },
     {
         "question": "What is the largest river in West Africa?",
-        "options": ["Congo River", "Niger River", "Zambezi River", "Limpopo"],
+        "options": [
+            "Congo River", "Niger River", "Zambezi River", "Limpopo River"
+        ],
         "correct_answer": "Niger River",
     },
     {
@@ -46,37 +58,86 @@ quiz_data = [
     },
     {
         "question": "What is the highest mountain in Africa?",
-        "options": ["Mount Kenya", "Mount Kilimanjaro", "Elgon", "Simien"],
+        "options": [
+            "Mount Kenya", "Mount Kilimanjaro", "Mount Elgon",
+            "Simien Mountains"],
         "correct_answer": "Mount Kilimanjaro",
     },
     {
         "question": "Which African country is known as the 'Giant of Africa'?",
-        "options": ["Nigeria", "South Africa", "Ghana", "Kenya"],
+        "options": [
+            "Nigeria", "South Africa", "Ghana", "Kenya"
+        ],
         "correct_answer": "Nigeria",
     },
     {
         "question": "Which African country is famous for the Victoria Falls?",
-        "options": ["Zambia", "Zimbabwe", "Botswana", "Mozambique"],
+        "options": [
+            "Zambia", "Zimbabwe", "Botswana", "Mozambique"
+        ],
         "correct_answer": "Zambia",
     },
     {
         "question": "What is the largest city in Africa by population?",
-        "options": ["Lagos", "Cairo", "Kinshasa", "Johannesburg"],
+        "options": [
+            "Lagos", "Cairo", "Kinshasa", "Johannesburg"
+        ],
         "correct_answer": "Lagos",
     },
     {
         "question": "Which African country is known as the 'Pearl of Africa'?",
-        "options": ["Uganda", "Ethiopia", "Nigeria", "Tunisia"],
+        "options": [
+            "Uganda", "Ethiopia", "Nigeria", "Tunisia"
+        ],
         "correct_answer": "Uganda",
     },
     {
         "question": "What is the largest island country in Africa?",
-        "options": ["Madagascar", "Seychelles", "Comoros", "Mauritius"],
+        "options": [
+            "Madagascar", "Seychelles", "Comoros", "Mauritius"
+        ],
         "correct_answer": "Madagascar",
     },
     {
         "question": "What is the largest country by land area in Africa?",
-        "options": ["Nigeria", "South Africa", "Algeria", "Sudan"],
+        "options": [
+            "Nigeria", "South Africa", "Algeria", "Sudan"
+        ],
+        "correct_answer": "Algeria",
+    },
+    {
+        "question": "What is the largest national park in Africa?",
+        "options": [
+            "Kruger National Park", "Serengeti National Park",
+            "Murchison Falls National Park", "Etosha National Park"
+        ],
+        "correct_answer": "Serengeti National Park",
+    },
+    {
+        "question": "Which African country is famous for its",
+        "question_contd": "cocoa production?",
+        "options": ["Côte d'Ivoire", "Ghana", "Nigeria", "Cameroon"],
+        "correct_answer": "Côte d'Ivoire",
+    },
+    {
+        "question": "Which African country is known as the",
+        "question_contd": "'Pearl of the Indian Ocean'?",
+        "options": ["Madagascar", "Comoros", "Seychelles", "Mauritius"],
+        "correct_answer": "Mauritius",
+    },
+    {
+        "question": "Which African nation, despite its proximity to European",
+        "question_contd": [
+            "colonies, maintained its",
+            "sovereignty during the Scramble for Africa?"
+        ],
+        "options": ["Kenya", "Uganda", "Somalia", "Morocco"],
+        "correct_answer": "Morocco"
+    },
+    {
+        "question": "Which African country successfully resisted colonization",
+        "question_contd": "by the French, despite several attempts?",
+        "options": ["Madagascar", "Senegal", "Algeria", "Tunisia"],
         "correct_answer": "Algeria",
     },
 ]
@@ -179,4 +240,4 @@ if __name__ == "__main__":
         restart_quiz()
     else:
         clear_screen()
-        print("Okay, maybe next time!")
+        print("\033[91mOkay, maybe next time!\033[0m")
